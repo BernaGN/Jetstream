9a69b5a99d5e1599979c3aa33818a45944c938f3
Berna <benano51@gmail.com>
2022-05-25 13:40:41 -0500

    Link productos

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/productos', Products::class)->name('dashboard');
+    Route::get('/productos', Products::class)->name('productos');
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
